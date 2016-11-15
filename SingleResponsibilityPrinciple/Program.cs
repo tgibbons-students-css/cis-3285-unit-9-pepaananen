6673f9ce8c3d7787ea61caff306a611821ba8901
PeterPaananen <pepaananen@gmail.com>
2016-11-14 22:38:05 -0600

    Req 407 Made url data provider

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -9,11 +9,11 @@ namespace SingleResponsibilityPrinciple
     {
         static void Main(string[] args)
         {
-            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
+            string url = "http://faculty.css.edu/tgibbons/trades4.txt";
 
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
-            var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            var tradeDataProvider = new URLTradeDataProvider(url);
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
             var tradeStorage = new AdoNetTradeStorage(logger);
